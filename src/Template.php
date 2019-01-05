33c6ace05ee4e0336f9d13052311d77544ada726
Aleksey Ryzhenkov <simposium@ya.ru>
2019-01-05 12:45:17 +0300

    no cdata

--- src/Template.php
+++ src/Template.php
@@ -8,11 +8,6 @@ class Template
         $tree     = static::lex($template);
         $html     = static::parse($tree);
 
-        if (app('config')->get('app.debug'))
-        {
-            $html = static::tidy($html);
-        }
-
         return $html;
     }
 
@@ -45,29 +40,5 @@ class Template
         }
 
         return $html;
-    }
-
-    static function tidy($html)
-    {
-        $needles = [
-            '~<style>*?>|(<script.*?>(?!\s*</script>))~',
-            '~</style>|((<script>\s*\K)</script>)~'
-        ];
-
-        $replacements = ['$0<![CDATA[', ']]>$0'];
-
-        $html = preg_replace($needles, $replacements, $html);
-
-        $settings = [
-            'indent' => true,
-            'input-xml' => true,
-            'escape-cdata' => true
-        ];
-
-        //$tidy = tidy_parse_string($html, $settings, 'utf8');
-
-        return $html;
-        
-        return preg_replace('~(</.+>|<.+/>|-->)(?=\n *<\w+)~m', "\$1\n", $tidy);
     }
 }
